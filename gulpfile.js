55207f75af89cad4deec94902692db595323679a
Eric Thornton <thornton.w.eric@gmail.com>
2015-03-19 09:11:28 -0700

    main layout and structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,39 @@
-var elixir = require('laravel-elixir');
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(function(mix) {
-    mix.less('app.less');
+var gulp = require('gulp');
+var concat = require('gulp-concat'); //combines files
+var uglify = require('gulp-uglify'); //minification
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer'); //automatically adds webkit
+ 
+gulp.task('sass', function () {
+    gulp.src('./public/scss/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+                browsers: ['last 5 versions'],
+                cascade: false
+            }))
+        .pipe(gulp.dest('./public/css'));
 });
+
+
+gulp.task('scripts', function() {
+
+  gulp.src([
+		'./bower_components/jquery/dist/jquery.js',
+  		'./bower_components/handlebars/handlebars.js',
+  		'./public/js/src/*.js'
+  	])
+    .pipe(concat('build.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js/'));
+
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+	gulp.watch(['./public/js/src/*.js'], ['scripts']);
+	gulp.watch(['./public/scss/*.scss'], ['sass']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['watch', 'scripts', 'sass']);
+
---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+$(function() {
+  	$('.btn-group').on('click', function() {
+  	  	$('.dropdown-menu').toggleClass('expand-btn-group')
+  	})
+})
