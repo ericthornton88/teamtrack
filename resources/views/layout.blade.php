21318251b0c2461dbf690cdf2bb587922f891107
Eric Thornton <thornton.w.eric@gmail.com>
2015-03-19 21:46:26 -0700

    feature boxes home page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,6 @@
 	<meta charset="UTF-8">
 	<title>Dueling Decisions</title>
 	<link rel="stylesheet" href="<?php echo asset('css/app.css')?>" type="text/css">
-
 	<link rel="stylesheet" href="<?php echo asset('css/styles.css')?>" type="text/css">
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
@@ -16,17 +15,14 @@
 <body>
 	
 	<header>
-		<div class="nav-logo"><img src="../img/muscleLogo.jpeg" alt=""></div>
-		<div class="nav-logo"></div>
-		<div class="btn-group open">
-		  <a class="btn btn-primary" href="#"><i class="fa fa-user fa-fw"></i> User</a>
-		  <ul class="dropdown-menu expand-btn-group">
-		    <li><a href="#"><i class="fa fa-pencil fa-fw"></i> Edit</a></li>
-		    <li><a href="#"><i class="fa fa-trash-o fa-fw"></i> Delete</a></li>
-		    <li><a href="#"><i class="fa fa-ban fa-fw"></i> Ban</a></li>
-		    <li class="divider"></li>
-		    <li><a href="#"><i class="i"></i> Make admin</a></li>
-		  </ul>
+		<div class="nav-container">
+			<div class="nav-logo"><img src="../img/muscleLogo.jpeg" alt=""></div>
+			<nav>
+				<a href="#">Demo</a>
+				<a href="#">Products</a>
+				<a href="#">About Us</a>
+				<a href="#">Login</a>
+			</nav>
 		</div>
 	</header>
 
@@ -37,7 +33,11 @@
 		@show
 
 	<footer>
-		
+		<div class="copyright">content</div>
+		<div class="footer-links">
+			<a href="#">Home</a>
+			<a href="#">Login/Register</a>
+		</div>
 	</footer>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,21 @@
 	</div>
 	<main>
 		<div class="main-container">
-			<aside>img</aside>
-			<article>Manage the statistics that matter. An easy to use dashboard provides clear direction on your strengths.</article>
+		<div class="feature-container">
+			<div class="img"><i class="fa fa-line-chart"></i></div>
+			<div class="title">Analytics</div>
+			<div class="content">On point visuals that give you the quick insights you need.</div>
+		</div>
+		<div class="feature-container">
+			<div class="img"><i class="fa fa-check"></i></div>
+			<div class="title">Simple</div>
+			<div class="content">Easy to use interface that still covers all your needs</div>
+		</div>
+		<div class="feature-container">
+			<div class="img"><i class="fa fa-question-circle"></i></div>
+			<div class="title">Support</div>
+			<div class="content">Easy to work with support staff that makes work flow</div>
+		</div>
 		</div>
 	</main>
 @endsection
